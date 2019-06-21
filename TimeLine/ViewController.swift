895f955ab3666f9549abdace5d54705efee157fe
alvin joseph valdez <alvin.consultant@platform-11.com>
2019-06-21 10:47:07 +0800

    Initial Commit

---
+++ TimeLine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeLine
+//
+//  Created by Alvin Joseph Valdez on 21/06/2019.
+//  Copyright © 2019 Alvin Joseph Valdez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
